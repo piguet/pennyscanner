cfee7d28818a5201b6ecbdcb249b5e3d05df4de4
piguet <freeableton@gmail.com>
2020-05-11 14:34:38 +0200

    Update single.py

--- single.py
+++ single.py
@@ -10,7 +10,7 @@ import json
 stock = input("Choose stock symbol:\n ")
 
 sent = vol = price = volchange = trend = perc = "--"
-print("Code\tFrequency\tWatchers\tPrice\tSentiment\tTrendScore\tVolume\tVol%\tPrice%")
+print("Code\tEarnings\tWatchers\tPrice\tSentiment\tTrendScore\tVolume\tVol%\tPrice%\tDirection")
 r = requests.get(url='https://api.stocktwits.com/api/2/streams/symbol/'+stock+'.json')
 json_data = json.loads(r.text)
 try:
@@ -61,6 +61,29 @@ try:
 except IndexError:
 	price="--"  
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+headers = {'User-Agent': user_agent}
+
+yho = requests.get(url='https://finance.yahoo.com/quote/'+stock+'', headers=headers)
+page_source = yho.content	
+
+soup = BeautifulSoup(page_source, "html.parser")
+#all_scripts = soup.find_all('script')
+script=str(soup)
+earn = re.search(r'<span data-reactid="159">(.*?)<\/span>', script)
+jsData = re.search(r'"shortTermOutlook":{"stateDescription":"(.*?)","direction":', script)
+
+
+
+
+try:
+	indic=jsData.group(1)
+except AttributeError:
+	indic:"--"
+try:
+	earn=earn.group(1)
+except AttributeError:
+	earn:"--"
 
 	 
 padding = ""
@@ -69,4 +92,4 @@ padding = ""
 if len(stock) < 4:
     padding = ' '
 
-print(str(stock) + padding + "\t" + str(stock) + padding + "\t \t" + str(watchers)+ padding + "\t \t" + str(price) + "\t" + str(sent)+ "\t\t"  + str(trend)+ "\t\t"  + str(vol) + "\t" + str(volchange) + "\t" + str(perc))
+print(str(stock) + padding + "\t" + str(earn) + padding + "\t" + str(watchers)+ padding + "\t \t" + str(price) + "\t" + str(sent)+ "\t\t"  + str(trend)+ "\t\t"  + str(vol) + "\t" + str(volchange) + "\t" + str(perc)+ "\t"+ str(indic))
